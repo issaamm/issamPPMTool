704fde5aadbacfa0dcf5950069abe22b4b371685
unknown <issam.fpn.info@gmail.com>
2019-09-27 16:58:56 +0100

    first commit

---
+++ ProjectMTool/src/main/java/com/issam/projectmanagment/ProjectMTool/ProjectMToolApplication.java
@@ -0,0 +1,13 @@
+package com.issam.projectmanagment.ProjectMTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMToolApplication.class, args);
+	}
+
+}
